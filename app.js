897c5d43b7cf800bd556dd1d971a940e4be75c10
TamerHussien <eng.tamerhussien@gmail.com>
2017-07-23 15:04:02 +0200

    finished and tested with postman

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 8000;
 var logger = require('morgan');
 var io = require('socket.io');
 
